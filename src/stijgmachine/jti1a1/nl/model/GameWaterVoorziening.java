a4c8d4f4a24245b19bfd7cd587a5fd33e6927b7f
m3talelite <jelle@braat.nu>
2013-05-07 14:02:17 +0200

    initial commit watervoorziening

---
+++ src/stijgmachine/jti1a1/nl/model/GameWaterVoorziening.java
@@ -0,0 +1,131 @@
+package stijgmachine.jti1a1.nl.model;
+
+import java.util.ArrayList;
+
+import stijgmachine.jti1a1.nl.objects.GameObject;
+import wiiusej.Wiimote;
+import wiiusej.wiiusejevents.physicalevents.ExpansionEvent;
+import wiiusej.wiiusejevents.physicalevents.IREvent;
+import wiiusej.wiiusejevents.physicalevents.MotionSensingEvent;
+import wiiusej.wiiusejevents.physicalevents.WiimoteButtonsEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.ClassicControllerInsertedEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.ClassicControllerRemovedEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.DisconnectionEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.GuitarHeroInsertedEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.GuitarHeroRemovedEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.NunchukInsertedEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.NunchukRemovedEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.StatusEvent;
+
+public class GameWaterVoorziening extends MiniGameLogic {
+
+	private ArrayList<GameObject> GameObjects = new ArrayList<GameObject>();
+	private boolean done = false;
+
+	public GameWaterVoorziening() {
+
+	}
+
+	@Override
+	public void onButtonsEvent(WiimoteButtonsEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onClassicControllerInsertedEvent(
+			ClassicControllerInsertedEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onClassicControllerRemovedEvent(
+			ClassicControllerRemovedEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onDisconnectionEvent(DisconnectionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onExpansionEvent(ExpansionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onGuitarHeroInsertedEvent(GuitarHeroInsertedEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onGuitarHeroRemovedEvent(GuitarHeroRemovedEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onIrEvent(IREvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onMotionSensingEvent(MotionSensingEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onNunchukInsertedEvent(NunchukInsertedEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onNunchukRemovedEvent(NunchukRemovedEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStatusEvent(StatusEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isDone() {
+		// TODO Auto-generated method stub
+		return done;
+	}
+
+	@Override
+	public ArrayList<GameObject> getObjects() {
+		// TODO Auto-generated method stub
+		return GameObjects;
+	}
+
+	@Override
+	public void giveMotes(Wiimote[] wiimotes) {
+		// TODO Auto-generated method stub
+		wiimotes[0].addWiiMoteEventListeners(this);
+	}
+
+	public void gameOver() {
+		done = true;
+	}
+
+}
