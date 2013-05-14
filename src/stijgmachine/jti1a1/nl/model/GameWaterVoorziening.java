480c92fbcd4b9034767edb0dc629b4269856b276
m3talelite <jelle@braat.nu>
2013-05-14 09:42:26 +0200

    14-5-2013

--- src/stijgmachine/jti1a1/nl/model/GameWaterVoorziening.java
+++ src/stijgmachine/jti1a1/nl/model/GameWaterVoorziening.java
@@ -1,5 +1,8 @@
 package stijgmachine.jti1a1.nl.model;
 
+import java.awt.Dimension;
+import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -25,10 +28,15 @@ public class GameWaterVoorziening extends MiniGameLogic {
 	private ArrayList<GameObject> GameObjects = new ArrayList<GameObject>();
 	private LinkedList<Boolean> routes = new LinkedList<Boolean>();
 	private boolean done,drag = false;
-	private int index = 1;
+	private int index = 0;
+	private Point2D cursorLocation;
+	private Point2D pipeLocation;
+	private Dimension size;
+	private boolean add = true;
 
 	public GameWaterVoorziening() {
 		GameObjects.add(new GameCursor());
+		this.size = new Dimension(20,20);
 	}
 
 	@Override
@@ -37,14 +45,46 @@ public class GameWaterVoorziening extends MiniGameLogic {
 		if(event.isButtonDownPressed()){
 			GameObjects.clear();
 			GameObjects.add(new GameCursor());
+			nextPipe();
+			index = 0;
 		}
+		if(event.isButtonRightPressed())
+			nextPipe();
+		if(event.isButtonLeftPressed())
+			lastPipe();
 		if(event.isButtonHomePressed())
 			System.exit(0);
 		if(event.isButtonAPressed())
-			GameObjects.add(new GamePipes(10, 10, index));
-		if(event.isButtonBPressed())
+			if(add){
+			addPipe(0, index++);
+			add = false;
 			drag = true;
-		drag = false;	
+			}
+		if(event.isButtonBPressed())
+		{}
+//		if(!add)
+//			add = true;
+//		drag = false;	
+	}
+	
+	public void nextPipe(){
+		add = true;
+		drag = false;
+		index ++;
+	}
+	
+	public void lastPipe(){
+		add = false;
+		drag = true;
+		index --;
+	}
+	
+	public Dimension getPipeBounds(){	
+		return null;
+	}
+	
+	public void movePipe(){
+		
 	}
 
 	@Override
@@ -87,9 +127,11 @@ public class GameWaterVoorziening extends MiniGameLogic {
 
 	@Override
 	public void onIrEvent(IREvent event) {
-		((GameCursor) GameObjects.get(0)).update(event.getAx(), event.getAy());
+		setCursorLocation(new Point2D.Double((int)event.getAx(),(int)event.getAy()));
+		setPipeLocation(new Point2D.Double((int)event.getAx(),(int)event.getAy()));
+		((GameCursor) GameObjects.get(0)).update((int)getCursorLocation().getX(),(int)getCursorLocation().getY());
 		if (drag)
-			((GamePipes) GameObjects.get(index)).update(event.getAx(), event.getAy());
+			setPipeLocation(new Point2D.Double((int)event.getAx(),(int)event.getAy()));//((GamePipes) GameObjects.get(index)).update(event.getAx(), event.getAy());
 	}
 
 	@Override
@@ -119,6 +161,7 @@ public class GameWaterVoorziening extends MiniGameLogic {
 	@Override
 	public void tick() {
 		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
@@ -139,23 +182,33 @@ public class GameWaterVoorziening extends MiniGameLogic {
 		wiimotes[0].addWiiMoteEventListeners(this);
 		wiimotes[0].activateIRTRacking();
 	}
+	
+	private void setCursorLocation(Point2D point){
+		cursorLocation = point;
+	}
+
+	private Point2D getCursorLocation(){
+		return cursorLocation;
+	}
+	
+	private void setPipeLocation(Point2D point){
+		pipeLocation = point;
+	}
+	
+	private Point2D getPipeLocation(){
+		return pipeLocation;
+	}
 
 	public void gameOver() {
 		done = true;
 	}
 
-	public double getCursorX() {
-		return 0;
-	}
-
-	public double getCursorY() {
-		return 0;
-	}
-
-	private void addPipe(int number) {
-		switch (number) {
+	private void addPipe(int number, int index) {
+		int numbers = number;
+		int i = index;
+		switch (numbers) {
 		case 0: /* straight horizontal pipe */
-			GameObjects.add(new stijgmachine.jti1a1.nl.objects.GamePipes(10,10,number));
+			GameObjects.add(new GamePipes((int)getCursorLocation().getX(),(int)getCursorLocation().getY(),i,(int)getPipeLocation().getX(),(int)getPipeLocation().getY(), size));
 			break;
 		case 1: /* straight vertical pipe */
 			break;
--- src/stijgmachine/jti1a1/nl/objects/GamePipes.java
+++ src/stijgmachine/jti1a1/nl/objects/GamePipes.java
@@ -1,12 +1,18 @@
 package stijgmachine.jti1a1.nl.objects;
 
+import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 
 public class GamePipes extends GameObject {
-	private int ID;
-	public GamePipes(int x, int y, int id) {
+	private int ID, xPosition,yPosition;
+	Dimension size;
+	public GamePipes(int x, int y, int id,int xPosition,int yPosition, Dimension size) {
 		super(x, y, GameObject.ABSOLUTE);
 		this.ID = id;
+		this.xPosition = xPosition;
+		this.yPosition = yPosition;
+		this.size = size;
 		// TODO Auto-generated constructor stub
 	}
 
@@ -14,10 +20,12 @@ public class GamePipes extends GameObject {
 	public void draw(Graphics2D g, int height, int width, int x2, int y2) {
 		// TODO Auto-generated method stub
 		switch(getID()){
-		case 0:
-			g.drawLine(10, 10, 20, 20);
+		case 0: //horizontal
+			Rectangle2D rect = new Rectangle2D.Double(xPosition, yPosition, size.getWidth(), size.getHeight());
+			g.draw(rect);
 			break;
-		case 1:
+		case 1: // vertical
+			g.drawLine(xPosition/2,yPosition/2,(int)size.getWidth()/2,(int)size.getHeight()/2);
 			break;
 		case 2:
 			break;
